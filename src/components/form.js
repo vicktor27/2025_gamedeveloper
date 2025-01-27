7ee5ad07a0e55784cb64dad864b60ff3f444b73a
JiameCruz <169116566+JiameCruz@users.noreply.github.com>
2025-01-27 13:06:34 -0600

    Second Commit

---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+function Form(props) {
+  return (
+    <>
+      <form action="">
+        <label htmlFor="">Nombre</label>
+        <input type="text"></input>
+        <label htmlFor="">Edad</label>
+        <input type="text"></input>
+        <label htmlFor="">Email</label>
+        <input type="text"></input>
+      </form>
+    </>
+  );
+}
+export default Form;
