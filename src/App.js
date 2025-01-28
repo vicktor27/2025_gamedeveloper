451e2f71c300a87790f02fdae5a69bba4556bf14
JiameCruz <169116566+JiameCruz@users.noreply.github.com>
2025-01-28 12:05:10 -0600

    Comit 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import HelloStudents from "./components/HelloStudents";
 import Form from "./components/Form";
 import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 
 function App() {
+  const [count, setCount] = useState;
+
+  // Función para incrementar el contador
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  // Función para decrementar el contador
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
   let welcomeMessage = "Hello Students!!!!";
 
   return (
-    <div className="App">
-      <HelloStudents p1="Erasmo" p2="Elizabeth" />
-      <Form name="Nombre" age="Edad" email="Correo Electrónico"></Form>
-      <Form name="Apellido" age="Dirección" email="Teléfono"></Form>
-      <Counter />
+    <>
+      <div className="App">
+        <HelloStudents p1="Erasmo" p2="Elizabeth" />
+        <Form name="Nombre" age="Edad" email="Correo Electrónico"></Form>
+        <Form name="Apellido" age="Dirección" email="Teléfono"></Form>
+        <Counter
+          count={count}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+        />
+      </div>
       <Footer count={count}></Footer>
-    </div>
+    </>
   );
 }
 
