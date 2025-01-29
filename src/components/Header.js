a7c3ff27793cfacdc85db166de19c56ade6ba438
alan barraza <169072418+Raydrack03@users.noreply.github.com>
2025-01-29 11:53:12 -0600

    reac

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return(
+        <>
+        <header style={styles}>
+        </header>
+        </>
+    
+    );
+};
+
+export default Header;
+
+const styles = {
+    maxWidth: "1200px"
+};
