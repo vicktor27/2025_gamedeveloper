0de8d2d933d1ea6c00e3f16879a7c12ccc41f00a
alan barraza <169072418+Raydrack03@users.noreply.github.com>
2025-02-05 11:55:00 -0600

    update

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ import Form from "./components/Form";
 import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Header></Header>
+        <Header></Header> 
+        <Main></Main>
         <Footer></Footer>
       </div>
     </>
   );
 }
 
-// Estilos (puedes eliminar este objeto si no lo usas)
-const styles = {
-  container: {
-    textAlign: "center",
-    marginTop: "50px",
-  },
-  title: {
-    fontSize: "24px",
-    marginBottom: "20px",
-  },
-};
+
+
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import "./Main.css";
+import Section from "./Section";
+
+const Main = () => {
+    return (
+        <>
+        <main>
+           <Section></Section> 
+        </main>
+        </>
+     );
+}
+ 
+export default Main;
